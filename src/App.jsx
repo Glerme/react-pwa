cdf66645467b97fd7b73512c71d478f1a8276535
Gui <guiggff@gmail.com>
2022-05-27 22:47:39 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./App.css";
+
+const App = () => {
+ 
+  return (
+    <div className="main-container">
+      <h1>Hello world</h1>
+    </div>
+  );
+};
+
+export default App;
